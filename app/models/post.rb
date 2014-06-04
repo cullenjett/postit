04fb053232af0a6f50bffb2a55090c54fe70734e
Cullen Jett <cullenjett@gmail.com>
2014-06-04 10:48:29 -0600

    Improve slug algorithm

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
   validates :url, presence: true, uniqueness: true
   validates :description, presence: true
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
   def total_votes
     self.up_votes - self.down_votes
@@ -30,7 +30,34 @@ class Post < ActiveRecord::Base
   end
 
 
-  def generate_slug
-    self.slug = self.title.gsub(" ", "-").downcase
+  def generate_slug!
+    the_slug = to_slug(self.title)
+    post = Post.find_by(slug: the_slug)
+
+    count = 2
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = Post.find_by(slug: the_slug)
+      count += 1
+    end
+
+    self.slug = the_slug.downcase
+  end
+
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0 
+      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
+  end
+
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub!(/\s*[^A-Za-z0-9]\s*/, '-') # replaces empty spaces and non-alpha-numeric characters with a '-'
+    str.gsub!(/-+/, '-') # if there are multiple '-', replaces them with only one '-'
+    str.downcase
   end
 end
