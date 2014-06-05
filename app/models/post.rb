79f160505d26bfa03c2b298cd83f1c36e972cc2e
Cullen Jett <cullenjett@gmail.com>
2014-06-05 12:08:00 -0600

    Extract slug logic to Sluggable module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   include Voteable
+  include Sluggable
+    sluggable_column :title
 
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
@@ -10,42 +12,4 @@ class Post < ActiveRecord::Base
   validates :url, presence: true, uniqueness: true
   validates :description, presence: true
 
-  before_save :generate_slug!
-
-
-  def to_param
-    self.slug
-  end
-
-
-  def generate_slug!
-    the_slug = to_slug(self.title)
-    post = Post.find_by(slug: the_slug)
-
-    count = 2
-    while post && post != self
-      the_slug = append_suffix(the_slug, count)
-      post = Post.find_by(slug: the_slug)
-      count += 1
-    end
-
-    self.slug = the_slug.downcase
-  end
-
-
-  def append_suffix(str, count)
-    if str.split('-').last.to_i != 0 
-      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
-    else
-      return str + "-" + count.to_s
-    end
-  end
-
-
-  def to_slug(name)
-    str = name.strip
-    str.gsub!(/\s*[^A-Za-z0-9]\s*/, '-') # replaces empty spaces and non-alpha-numeric characters with a '-'
-    str.gsub!(/-+/, '-') # if there are multiple '-', replaces them with only one '-'
-    str.downcase
-  end
 end
