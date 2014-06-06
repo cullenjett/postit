eb67a5770d6cfe74223881e6de53d227c6218388
Cullen Jett <cullenjett@gmail.com>
2014-06-06 10:08:49 -0600

    Add roles to users and time zone functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,5 +29,16 @@ class ApplicationController < ActionController::Base
       flash[:error] = "You're not allowed to do that."
       redirect_to root_path
     end
+  end
+
+
+  def require_admin
+    access_denied unless logged_in? && current_user.admin?
   end 
+
+
+  def access_denied
+    flash[:error] = "Sorry, you don't have permission to do that."
+    redirect_to :back
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,17 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :vote]
   before_action :require_user, except: [:index, :show]
+  before_action :require_creator, only: [:edit, :update]
 
 
   def index 
     @posts = Post.all.sort_by{ |e| e.total_votes }.reverse
+
+    respond_to do |format|
+      format.html {}
+
+      format.json { render json: @posts }
+    end
   end
 
 
@@ -75,4 +82,10 @@ class PostsController < ApplicationController
   def set_post
     @post = Post.find_by(slug: params[:id])
   end
+
+
+  def require_creator
+    access_denied unless logged_in? && (current_user.admin? || current_user == @post.creator)
+  end
+
 end
