30454b94644a525c4ca259445f81e855502a5dc4
Cullen Jett <cullenjett@gmail.com>
2014-06-08 16:52:32 -0600

    Disable turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,6 @@
 //= require_tree .
 //= require bootstrap
 
-
 $(document).ready(function() {
   $("a#two_factor_phone").popover({
     trigger: 'hover',
@@ -24,3 +23,5 @@ $(document).ready(function() {
     content: "We use your phone number for 2 factor authenticaiton. 2-factor authentication provides an extra layer of security when logging in to secure systems. If you provide a phone number here, you will receive a text message with a pin number to access your account. To skip 2 factor authenticaiton and login with just a username and password, leave this field blank. US numbers only."
   });
 });
+
+
